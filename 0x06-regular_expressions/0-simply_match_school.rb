4ed2f8f4bc39cef2fee1b37c4f7a5d4b134a8556
Excelnoel <mannymacadex@gmail.com>
2024-01-30 12:17:30 -0800

    0-simply_match_school.rb

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,20 +1,3 @@
 #!/usr/bin/env ruby
 #regular expression must match School
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$0} <string>"
-  exit 1
-end
-
-# Get the argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Use the regular expression to scan all occurrences
-match_results = input_string.scan(pattern)
-
-# Output the result
-puts match_results.empty? ? "$" : "#{match_results.join}$"
+puts ARGV[0].scan(/School/).join
